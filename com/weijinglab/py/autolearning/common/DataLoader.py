6c0c2539999cb6c5c564e2bd4c09a7fca9109df7
huangweijing <huangweijinglove@gmail.com>
2017-06-18 17:39:43 +0900

    just a test

--- com/weijinglab/py/autolearning/common/DataLoader.py
+++ com/weijinglab/py/autolearning/common/DataLoader.py
@@ -1,6 +1,7 @@
 import configparser
 import logging
 import os
+import urllib.request
 
 
 # MINSTのデータを読み込む
@@ -11,6 +12,8 @@ class TrainingDataLoader:
     __train_label = []
     __test_label = []
 
+    __config = configparser.ConfigParser()
+
     PROPERTY_SECTION_NAME = "DataLoaderConfig"
     PROPERTY_URLBASE_NAME = "url_base"
     PROPERTY_TRAIN_DATA_NAME = "train_data"
@@ -18,23 +21,39 @@ class TrainingDataLoader:
     PROPERTY_TRAIN_LABEL_NAME = "train_label"
     PROPERTY_TEST_LABEL_NAME = "test_label"
 
+    MINST_DATA_DIR = "minst_datafiles"
+
     def __init__(self, property_file="./auto_learning.properties"):
         logging.info("TEST")
         # try:
-        config = configparser.ConfigParser()
-        config.read(property_file)
+        self.__config.read(property_file)
 
-        self.__urlbase = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_URLBASE_NAME)
-        self.__train_data = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TRAIN_DATA_NAME)
-        self.__test_data = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TEST_LABEL_NAME)
-        self.__train_label = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TRAIN_LABEL_NAME)
-        self.__test_label = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TEST_LABEL_NAME)
+        self.__urlbase = self.__config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_URLBASE_NAME)
+        # self.__train_data = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TRAIN_DATA_NAME)
+        # self.__test_data = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TEST_LABEL_NAME)
+        # self.__train_label = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TRAIN_LABEL_NAME)
+        # self.__test_label = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TEST_LABEL_NAME)
 
     def load(self, filename):
         pass
 
-    def __download(self):
-        pass
+    def __download(self, filename):
+        file_path = "%s/%s/%s" % (os.path.dirname(os.path.abspath(__file__)), self.MINST_DATA_DIR, filename)
+        if os.path.exists(file_path):
+            return
+        file_url_path = "%s%s" % (self.__urlbase, filename)
+        logging.info("Downloading minst data from " + file_url_path + "...")
+        print("Downloading minst data from %s" % file_url_path)
+        urllib.request.urlretrieve(file_url_path, file_path)
+        logging.info("Download Completed!")
+
+    def download_all_stuff(self):
+        file_to_be_downloaded = [self.__config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TRAIN_DATA_NAME)
+                                 , self.__config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TEST_DATA_NAME)
+                                 , self.__config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TRAIN_LABEL_NAME)
+                                 , self.__config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TEST_LABEL_NAME)]
+        for file in file_to_be_downloaded:
+            self.__download(file)
 
     def get_train_data(self):
         return self.__train_data
@@ -48,5 +67,10 @@ class TrainingDataLoader:
     def get_test_label(self):
         return self.__test_label
 
+    def download_test(self):
+        self.__download(self.__config.get(self.PROPERTY_SECTION_NAME, TrainingDataLoader.PROPERTY_TRAIN_DATA_NAME))
 
-TrainingDataLoader("%s%s" % (os.path.dirname(os.path.abspath(__file__)), "/auto_learning.properties"))
+
+tdl = TrainingDataLoader("%s%s" % (os.path.dirname(os.path.abspath(__file__)), "/auto_learning.properties"))
+tdl.download_all_stuff()
+# tdl.download(config.get(self.PROPERTY_SECTION_NAME, TrainingDataLoader.PROPERTY_TRAIN_DATA_NAME))
