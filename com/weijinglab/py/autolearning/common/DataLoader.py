55b1a4c00f126e28765b2325cf7161de82e5c91b
huangweijing <huangweijinglove@gmail.com>
2017-06-18 16:52:26 +0900

    just a test

--- com/weijinglab/py/autolearning/common/DataLoader.py
+++ com/weijinglab/py/autolearning/common/DataLoader.py
@@ -2,36 +2,33 @@ import configparser
 import logging
 import os
 
+
 # MINSTのデータを読み込む
 class TrainningDataLoader:
-
     __urlbase = ""
     __train_data = []
     __test_data = []
     __train_label = []
     __test_label = []
 
+    PROPERTY_SECTION_NAME = "DataLoaderConfig"
+    PROPERTY_URLBASE_NAME = "url_base"
+    PROPERTY_TRAIN_DATA_NAME = "train_data"
+    PROPERTY_TEST_DATA_NAME = "test_data"
+    PROPERTY_TRAIN_LABEL_NAME = "train_label"
+    PROPERTY_TEST_LABEL_NAME = "test_label"
+
     def __init__(self, property_file):
         logging.info("TEST")
         # try:
-        config = configparser.RawConfigParser().read(property_file)
-        __urlbase = config.get("DataloaderConfig", "url_base")
-        __train_data = config.get("DataloaderConfig", "train_data")
-        __test_data =config.get("DataloaderConfig", "test_data")
-        __train_label =config.get("DataloaderConfig", "train_label")
-        __test_label = config.get("DataloaderConfig", "test_label")
-        #except BaseException:
-            #raise BaseException()
-            #pass
-            #logging("configuration loading error!")
-        # else:
-        #     print("okay!")
-        #     logging("configuration loaded!")
-        #     logging("DataloaderConfig.usr_base" + __urlbase)
-        #     logging("DataloaderConfig.train_data" + __train_data)
-        #     logging("DataloaderConfig.test_data" + __test_data)
-        #     logging("DataloaderConfig.train_label" + __train_label)
-        #     logging("DataloaderConfig.test_label" + __test_label)
+        config = configparser.ConfigParser()
+        config.read(property_file)
+
+        self.__urlbase = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_URLBASE_NAME)
+        self.__train_data = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TRAIN_DATA_NAME)
+        self.__test_data = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TEST_LABEL_NAME)
+        self.__train_label = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TRAIN_LABEL_NAME)
+        self.__test_label = config.get(self.PROPERTY_SECTION_NAME, self.PROPERTY_TEST_LABEL_NAME)
 
     def load(self, filename):
         pass
@@ -51,7 +48,5 @@ class TrainningDataLoader:
     def getTestLabel(self):
         return self.__test_label
 
-print(os.path.exists("%s%s" % (os.path.dirname(os.path.abspath(__file__)), "/autolearning.properties")))
-TrainningDataLoader("%s%s" % (os.path.dirname(os.path.abspath(__file__)), "/autolearning.properties"))
 
-#print("%s%s" % (os.path.dirname(os.path.abspath(__file__)), "/autolearning.properties"))
+TrainningDataLoader("%s%s" % (os.path.dirname(os.path.abspath(__file__)), "/autolearning.properties"))
